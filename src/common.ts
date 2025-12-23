c0741682e12b03ddf54f3a58492da991ab2d5c1b
xtaltia <aletabix@pm.me>
2025-12-23 01:45:22 +0000

    move default alea to separate file

--- src/common.ts
+++ src/common.ts
@@ -6,10 +6,5 @@ export {
 	createAleaFromFunc,
 } from "./internal/factories.js";
 
-export { charsets } from "./internal/charsets.js";
-
-/**
- * An Alea instance that uses Math.random() as a source
- */
-export const alea = new Alea(Math.random);
-
+export { charsets } from "./internal/charsets";
+export { alea } from "./internal/mathalea.js";
---
+++ src/internal/mathalea.ts
@@ -0,0 +1,6 @@
+import { Alea } from "./alea.js";
+
+/**
+ * An Alea instance that uses Math.random() as a source
+ */
+export const alea = new Alea(Math.random);
